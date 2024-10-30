c4b97d6eb2350d109287c7a2ff19f4a0c065eb0a
NataliaMuratova1979 <mno@mail.ru>
2024-10-30 12:22:42 +0300

    установлена react-router-dom

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,19 @@
-import { ConstructorPage } from '@pages';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
+
 import '../../index.css';
+
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
