74aa76d3cb4b9562c1786c5ac74f5e2bc0867968
NataliaMuratova1979 <mno@mail.ru>
2024-11-05 14:11:24 +0300

    работает удаление начинки

--- src/components/burger-constructor-element/burger-constructor-element.tsx
+++ src/components/burger-constructor-element/burger-constructor-element.tsx
@@ -4,7 +4,8 @@ import { BurgerConstructorElementProps } from './type';
 import { useDispatch } from 'react-redux';
 import {
   moveIngredientUp,
-  moveIngredientDown
+  moveIngredientDown,
+  removeIngredient
 } from '../../services/burgerSlice';
 
 export const BurgerConstructorElement: FC<BurgerConstructorElementProps> = memo(
@@ -12,15 +13,19 @@ export const BurgerConstructorElement: FC<BurgerConstructorElementProps> = memo(
     const dispatch = useDispatch();
 
     const handleMoveDown = () => {
+      console.log('Попытка переместить ингредиент вниз: ${ingredient.id}');
       dispatch(moveIngredientDown(ingredient.id));
     };
 
     const handleMoveUp = () => {
+      console.log('Попытка переместить ингредиент вверх: ${ingredient.id}');
       dispatch(moveIngredientUp(ingredient.id));
     };
 
     const handleClose = () => {
-      // логика для удаления ингредиента из конструктора
+      // Реализуйте логику для удаления ингредиента из конструктора
+      dispatch(removeIngredient(ingredient.id));
+      console.log('Удаление ингредиента: ${ingredient.id}');
     };
 
     return (
